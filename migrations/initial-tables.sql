69a6627d4caf5db57e652b24052ddccff1264afa
paufase <paufajardo00@gmail.com>
2023-07-02 11:26:33 +0200

    feat: Added review table

--- migrations/initial-tables.sql
+++ migrations/initial-tables.sql
@@ -1,18 +1,32 @@
 create table rating (
-    id integer not null primary key,
+    id bigint not null primary key,
     username varchar(255) not null,
-    game_id integer not null,
+    game_id bigint not null,
     rating integer not null,
     created_at timestamp not null,
-    updated_at timestamp not null,
-    constraint fk_game_id foreign key (game_id) references game (id),
+    updated_at timestamp,
     constraint unique_rating_per_game unique (username, game_id)
 );
 
 create table game (
-    id integer not null primary key,
+    id bigint not null primary key,
     name varchar(255) not null,
     year integer not null,
     created_at timestamp not null,
-    updated_at timestamp not null
+    updated_at timestamp,
+    constraint unique_game_name_year unique (name, year)
 );
+
+create table review (
+    id bigint not null primary key,
+    username varchar(255) not null,
+    game_id bigint not null,
+    review_description text not null,
+    rating_id bigint not null,
+    created_at timestamp not null,
+    updated_at timestamp,
+    constraint fk_review_rating_id foreign key (rating_id) references rating (id),
+    constraint unique_review_per_game unique (username, game_id)
+);
+
+alter table rating add constraint fk_rating_game_id foreign key (game_id) references game (id);
